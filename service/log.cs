e51f96fda51bc8c81c667b04bebe9871bdd1bbc4
unknown <biter1145@outlook.com>
2024-08-05 20:50:51 +0800

    Update log logic

--- service/log.cs
+++ service/log.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Reflection.Metadata;
+using System.ComponentModel.Design;
 
 namespace bedrock.NetHub.service
 {
     public partial class Logger(string nameSpace)
     {
         private readonly string nameSpace = nameSpace;
-        private readonly Dictionary<string,string> colorMap = new Dictionary<string, string> {
+        private readonly Dictionary<string,string> colorMap = new(){
             { "§0", "\x1b{30m" }, // Black
             { "§1", "\x1b{34m" }, // Dark Blue
             { "§2", "\x1b{32m" }, // Dark Green
@@ -27,6 +29,27 @@ namespace bedrock.NetHub.service
             { "§e", "\x1b{93m" }, // Yellow
             { "§f", "\x1b{97m" }, // White
             { "§r", "\x1b{0m"  }, // return plain text
+        };
+
+        private readonly Dictionary<string, ConsoleColor> colorReference = new()
+        {
+            { "§0", ConsoleColor.Black }, // Black
+            { "§1", ConsoleColor.DarkBlue }, // Dark Blue
+            { "§2", ConsoleColor.DarkGreen }, // Dark Green
+            { "§3", ConsoleColor.DarkCyan }, // Dark Aqua
+            { "§4", ConsoleColor.DarkRed }, // Dark Red
+            { "§5", ConsoleColor.DarkMagenta }, // Dark Purple
+            { "§6", ConsoleColor.Yellow }, // Gold
+            { "§7", ConsoleColor.Gray }, // Gray
+            { "§8", ConsoleColor.DarkGray }, // Dark Gray
+            { "§9", ConsoleColor.Blue }, // Blue
+            { "§a", ConsoleColor.Green }, // Green
+            { "§b", ConsoleColor.Cyan }, // Aqua
+            { "§c", ConsoleColor.Red }, // Red
+            { "§d", ConsoleColor.Magenta }, // Light Purple
+            { "§e", ConsoleColor.Yellow }, // Yellow
+            { "§f", ConsoleColor.White }, // White
+            { "§r", ConsoleColor.White  }, // return plain text
         };
         public string GetNameSpace()
         {
@@ -50,7 +73,6 @@ namespace bedrock.NetHub.service
         }
 
         public void Info(string message, string timeString = null) {
-            message = ReplaceMinecraftColors(message);
             if (message.Contains('\n'))
             {
                 foreach (string i in message.Split("\r*\n"))
@@ -65,7 +87,6 @@ namespace bedrock.NetHub.service
         }
         public void InfoFormat(string format, params object[] args) {
             string formated = string.Format("[{0}][{1}]", GetFormatTime(), nameSpace) + string.Format(format, args);
-            formated = ReplaceMinecraftColors(formated);
             if (formated.Contains('\n'))
             {
                 foreach (string i in formated.Split('\n'))
@@ -81,14 +102,68 @@ namespace bedrock.NetHub.service
 
         private void Log(string nameSpace,string content,string timeString = null)
         {
-            if (timeString == null)
+            Console.WriteLine(string.Format("[{0}][{1}] ", timeString ?? GetFormatTime(), nameSpace) + GetLoggerRegex().Replace(content,""));
+            /*
+            string[] SplitedContent = GetLoggerRegex().Split(content);
+            MatchCollection maches = GetLoggerRegex().Matches(content);
+            if (maches == null || maches.Count == 0)
             {
-                Console.WriteLine(string.Format("[{0}][{1}] ", GetFormatTime(), nameSpace) + content);
+                Console.WriteLine(string.Format("[{0}][{1}] ", timeString ?? GetFormatTime(), nameSpace) + content);
             }
             else
             {
-                Console.WriteLine(string.Format("[{0}][{1}] ", timeString, nameSpace) + content);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(string.Format("[{0}][{1}] ", timeString ?? GetFormatTime(), nameSpace));
+                if (content.StartsWith('§'))
+                {
+                    for (int i = 0;i < maches.Count; i++)
+                    {
+                        Console.Write("\n" + maches[i].Value + "----" + SplitedContent[i] + "\n");
+                        if (!colorReference.TryGetValue(maches[i].Value,out ConsoleColor cc))
+                        {
+                            Console.Write(SplitedContent[i]);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = cc;
+                            Console.Write(SplitedContent[i]);
+                        }
+                    }
+                    Console.Write('\n');
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (SplitedContent.Length == maches.Count + 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(SplitedContent[0]);
+                    for (int i = 0; i < maches.Count; i++)
+                    {
+                        Console.Write("\n" + maches[i].Value + "----" + SplitedContent[i + 1] + "\n");
+                        if (!colorReference.TryGetValue(maches[i].Value, out ConsoleColor cc))
+                        {
+                            Console.Write(SplitedContent[i + 1]);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = cc;
+                            Console.Write(SplitedContent[i + 1]);
+                        }
+                    }
+                    Console.Write('\n');
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.Write(GetLoggerRegex().Replace(content, "") + "\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
             }
+            */
+        }
+
+        public static void GeneralLog(string nameSpace,string content)
+        {
+            Console.WriteLine(string.Format("[{0}][{1}] ", GetFormatTime(), nameSpace) + content);
         }
 
         [GeneratedRegex("§[0-9a-fr]")]
